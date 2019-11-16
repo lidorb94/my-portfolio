4322987b82bb707b108b23bc47a71fcd72549e58
Lidor Batino <lidorb28@gmail.com>
2019-11-16 20:06:12 +0200

    website-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./round-200x200-shadow.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+        <img src={logo} width="200px" height="200px"></img>
+
+        <div className="Content">
+          <div className="Header">
+            <div className="Headline">
+              <h1>Lidor Batino</h1>
+              <h4>Junior Front-end Developer</h4>
+            </div>
+            <table className="Icons">
+              <thead>
+                <tr>
+                  <td>
+                    <a href="http://www.facebook.com" target="_blank">
+                      <p className="Facebook"></p>
+                    </a>
+                  </td>
+                  <td>
+                    <a href="https://github.com/lidorb94" target="_blank">
+                      <p className="Github"></p>
+                    </a>
+                  </td>
+                  <td>
+                    <a
+                      href="https://www.linkedin.com/in/lidor-batino-574789172/"
+                      target="_blank"
+                    >
+                      <p className="Linkedin"></p>
+                    </a>
+                  </td>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <p>
+            I'm an extremely passionate developer <br></br>
+            who always love to learn new things and evolve. <br></br>
+            <br></br>
+            Specialize in HTML5, CSS3, JavaScript and ReactJS<br></br>
+            Love what i do, and love to do it &#128578;
+          </p>
+          <p>
+            Contact me anytime:
+            <a href="mailto:lidorb28@gmail.com"> lidorb28@gmail.com</a>
+          </p>
+        </div>
+
     </div>
   );
 }
